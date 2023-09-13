f9e3659ba6ed602cea843a98f522834aa19f303c
decagon <chiorlujack@gmail.com>
2023-09-14 00:14:46 +0100

    initial commit

--- Task Management Project/Backend/src/controller/adminController/getAllPatients.ts
+++ Task Management Project/Backend/src/controller/adminController/getAllPatients.ts
@@ -4,7 +4,6 @@ import Patient, { PatientDocument } from "../../model/patient";
 export const getAllPatient = async (req: Request, res: Response) => {
   try {
     const patients: PatientDocument[] = await Patient.find().exec();
-
     res.status(200).json(patients);
   } catch (error) {
     console.error(error);
