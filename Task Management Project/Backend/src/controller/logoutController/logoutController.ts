a659140acdbc2fd02cc6ee513b2e50fbe9922d6e
decagon <chiorlujack@gmail.com>
2023-09-14 00:15:40 +0100

    logout

--- Task Management Project/Backend/src/controller/logoutController/logoutController.ts
+++ Task Management Project/Backend/src/controller/logoutController/logoutController.ts
@@ -1,6 +1,5 @@
 import { Request, Response } from "express";
 
-// *********************************************************************** //
 // Logout
 export const logout = async (req: Request, res: Response) => {
   try {
